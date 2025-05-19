2b6ebab645e2404eab130e60eda1ccb45a565b2b
Serine Galstyan <serine.galstyan98@gmail.com>
2025-05-19 15:00:57 +0400

    small fix of array syntax and area type

--- src/main/java/am/picsartacademy/oop_homework_1/RightTriangle.java
+++ src/main/java/am/picsartacademy/oop_homework_1/RightTriangle.java
@@ -6,7 +6,7 @@ public class RightTriangle extends Triangle {
     public double area(double firstSide, double secondSide, double thirdSide) {
         //keeping triangle sides in an area to find out which side is the altitude, and which ones are the small sides
         double[] array = {firstSide, secondSide, thirdSide};
-        double smallSides[] = new double[2]; //creating an array to add there small sides
+        double[] smallSides = new double[2]; //creating an array to add there small sides
 
         //getting and keeping the altitude in 'longest' variable and small sides in 'smallSides[]' array
         double longest = array[0];
@@ -26,7 +26,7 @@ public class RightTriangle extends Triangle {
         //else printing that the triangle was not right-angled
         if ((pow(longest, 2) == (pow(smallSides[0], 2) + pow(smallSides[1], 2)))) {
             System.out.print("Area of the right-angled triangle with given sides is ");
-            double area = 0.5 * (smallSides[0] * smallSides[1]);
+            area = 0.5 * (smallSides[0] * smallSides[1]);
             return area;
         }
          else {
